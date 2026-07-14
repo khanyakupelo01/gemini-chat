108001a9e33d334ce9a9a22c268080d25ddacbf7
Khanya Kupelo <58774118+khanyakupelo01@users.noreply.github.com>
2026-07-14 22:50:51 +0200

    chore: add ESLint and Prettier for code quality and formatting
    refactor: update animals route to use consistent formatting and remove unnecessary semicolons

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-const { getElementById, getIndexById, updateElement,
-  seedElements, createElement } = require('./utils');
+const {
+  getElementById,
+  getIndexById,
+  updateElement,
+  seedElements,
+  createElement
+} = require('./utils')
 
 // Use static server to serve the Express Yourself Website
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // Import and mount the expressionsRouter
-const expressionsRouter = require('./routes/expressions.js');
-app.use('/expressions', expressionsRouter);
+const expressionsRouter = require('./routes/expressions.js')
+app.use('/expressions', expressionsRouter)
 
 // Import and mount the expressionsRouter
-const animalsRouter = require('./routes/animals.js');
-app.use('/animals', animalsRouter);
+const animalsRouter = require('./routes/animals.js')
+app.use('/animals', animalsRouter)
 
 // export app for use in main.js and for testing
 module.exports = {
   app
-};
+}
--- routes/animals.js
+++ routes/animals.js
@@ -1,59 +1,64 @@
-const express = require('express');
+const express = require('express')
 
-const { getElementById, getIndexById, updateElement,
-  seedElements, createElement } = require('../utils');
+const {
+  getElementById,
+  getIndexById,
+  updateElement,
+  seedElements,
+  createElement
+} = require('../utils')
 
-let animals = [];
-seedElements(animals, 'animals');
+let animals = []
+seedElements(animals, 'animals')
 
-animalsRouter = express.Router();
+animalsRouter = express.Router()
 
 // Get all animals
 animalsRouter.get('/', (req, res, next) => {
-  res.send(animals);
-});
+  res.send(animals)
+})
 
 // Get a single animal
 animalsRouter.get('/:id', (req, res, next) => {
-  const animal = getElementById(req.params.id, animals);
+  const animal = getElementById(req.params.id, animals)
   if (animal) {
-    res.send(animal);
+    res.send(animal)
   } else {
-    res.status(404).send();
+    res.status(404).send()
   }
-});
+})
 
 // Create an animal
 animalsRouter.post('/', (req, res, next) => {
-  const receivedAnimal = createElement('animals', req.query);
+  const receivedAnimal = createElement('animals', req.query)
   if (receivedAnimal) {
-    animals.push(receivedAnimal);
-    res.status(201).send(receivedAnimal);
+    animals.push(receivedAnimal)
+    res.status(201).send(receivedAnimal)
   } else {
-    res.status(400).send();
+    res.status(400).send()
   }
-});
+})
 
 // Update an animal
 animalsRouter.put('/:id', (req, res, next) => {
-  const animalIndex = getIndexById(req.params.id, animals);
+  const animalIndex = getIndexById(req.params.id, animals)
   if (animalIndex !== -1) {
-    updateElement(req.params.id, req.query, animals);
-    res.send(animals[animalIndex]);
+    updateElement(req.params.id, req.query, animals)
+    res.send(animals[animalIndex])
   } else {
-    res.status(404).send();
+    res.status(404).send()
   }
-});
+})
 
 // Delete a single animal
 animalsRouter.delete('/:id', (req, res, next) => {
-  const animalIndex = getIndexById(req.params.id, animals);
+  const animalIndex = getIndexById(req.params.id, animals)
   if (animalIndex !== -1) {
-    animals.splice(animalIndex, 1);
-    res.status(204).send();
+    animals.splice(animalIndex, 1)
+    res.status(204).send()
   } else {
-    res.status(404).send();
+    res.status(404).send()
   }
-});
+})
 
-module.exports = animalsRouter;
+module.exports = animalsRouter
